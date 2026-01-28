6facfca7b2f3419d6944ff9de31257a90c59e163
vmro2002 <amro_official@hotmail.com>
2026-01-28 23:14:54 +0800

    problem2: update select modal token hover style

--- src/problem2/src/components/TokenSelectModal.tsx
+++ src/problem2/src/components/TokenSelectModal.tsx
@@ -50,7 +50,7 @@ export const TokenSelectModal: React.FC<TokenSelectModalProps> = ({
                 }}
                 className={cn(
                   "w-full flex items-center gap-3 p-3 rounded-xl",
-                  "hover:bg-secondary/10 transition-all duration-200",
+                  "hover:bg-secondary/60 transition-all duration-200",
                   selectedCurrency === token.currency &&
                     "bg-primary/10 border border-primary"
                 )}
